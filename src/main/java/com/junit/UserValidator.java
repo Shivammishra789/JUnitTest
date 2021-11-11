c5bd68f0d992fe19275e01f3a1a028aa2dd6cc95
Shivammishra789 <shivammishra7789@gmail.com>
2021-11-11 12:41:44 +0530

    ADDED: junit testing for first name

--- src/main/java/com/junit/UserValidator.java
+++ src/main/java/com/junit/UserValidator.java
@@ -1,5 +1,17 @@
 package com.junit;
 
+import java.util.regex.Pattern;
+
 public class UserValidator {
 
+	static String regex;
+	static Pattern pattern;
+
+	//method for validation of user first name using regex
+	public boolean validateFirstName(String firstName) {
+		// regex pattern 
+		regex = "^[A-Z]{1}[a-z]{2,}";
+		pattern = Pattern.compile(regex);
+		return pattern.matcher(firstName).matches();	
+	}
 }
--- src/test/java/com/junit/UserValidatorTest.java
+++ src/test/java/com/junit/UserValidatorTest.java
@@ -1,5 +1,21 @@
 package com.junit;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UserValidatorTest {
 
+	@Test 
+	public void givenFirstName_WhenProper_ShouldReturn_True() {
+		UserValidator validator = new UserValidator();
+		boolean result = validator.validateFirstName("Shivam");
+		Assert.assertTrue(result);
+	}
+	
+	@Test 
+	public void givenFirstName_WhenNotProper_ShouldReturn_False() {
+		UserValidator validator = new UserValidator();
+		boolean result = validator.validateFirstName("sh");
+		Assert.assertFalse(result);
+	}
 }
